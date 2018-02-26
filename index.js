5a7fdb3a29aec3e2426148556e72a515298ed678
Lance Barney <lbarney@littleappletech.com>
2018-02-25 18:23:33 -0700

    Very basic chat functionality with Socket.io

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+
   console.log('a user connected');
   socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
     console.log('message: '+ msg);
   });
   socket.on('disconnect', function(){
